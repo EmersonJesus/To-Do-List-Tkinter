967a705e7022dcd2f85e672ae342cd5aca9fa25a
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-17 12:59:54 -0300

    v0.0

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import *
+
+root=Tk()
+root.title("To-Do-List")
+root.geometry("400x650+400+100")
+root.resizable(False, False)
+
+lista_tarefas = []
+
+root.mainloop()
