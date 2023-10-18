9016f6a0a9acf3f1413e46ad4b2d77ab5ddb231e
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-18 13:12:11 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,13 +4,15 @@ fundo = '#242323'
 
 janela = Tk()
 janela.title('Label')
-janela.geometry('250x250')
+largura = 500
+altura = 250
+janela.geometry(f'{str(largura)}x{str(altura)}')
 janela.config(bg=fundo)
 janela.resizable(width=False, height=False)
-label_nome = Label(janela, width=10, height=2, text='Nome:', font=('Arial 15 bold'), bg=fundo, fg='white')
-label_nome.grid(row=0, column=0)
-label_idade = Label(janela, width=10, height=2, text='Idade:', font=('Arial 15 bold'), bg=fundo, fg='white')
-label_idade.grid(row=1, column=0)
-label_pais = Label(janela, width=10, height=2, text='País:', font=('Arial 15 bold'), bg=fundo, fg='white')
-label_pais.grid(row=2, column=0)
+label_nome = Label(janela, width=largura, height=2, text='Nome:', font=('Arial 15 bold'), bg=fundo, fg='white')
+label_nome.pack()
+label_idade = Label(janela, width=largura, height=2, text='Idade:', font=('Arial 15 bold'), bg=fundo, fg='white')
+label_idade.pack()
+label_pais = Label(janela, width=largura, height=2, text='País:', font=('Arial 15 bold'), bg=fundo, fg='white')
+label_pais.pack()
 janela.mainloop()
