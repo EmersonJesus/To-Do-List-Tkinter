d5bd39ef1ad211e84f814ee258d187e04fada2ab
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-19 12:42:55 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -9,6 +9,6 @@ altura = 250
 janela.geometry(f'{str(largura)}x{str(altura)}')
 janela.config(bg=fundo)
 janela.resizable(width=False, height=False)
-botao = Button(janela, width=10, height=2,text="Clica Aqui!", relief='flat', fg='red')
-botao.place(x=10, y=10)
+botao = Button(janela, width=10, height=2,text="Clica Aqui!", relief='flat', fg='white', bg='black')
+botao.place(x=largura//2.5, y=altura//2.5)
 janela.mainloop()
