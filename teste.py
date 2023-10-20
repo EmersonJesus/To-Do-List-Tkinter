0d12197f62a3b5fa25c730634b5f00f46e257a18
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-20 12:53:38 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -17,10 +17,18 @@ label_nome.grid(row=0, column=0)
 nome = Entry(janela, width=10, font=('Arial 10'))
 nome.grid(row=0, column=1, padx=10, pady=5)
 
+# Idade -------------------------------------------------
+label_idade = Label(janela, width=10, height=2, text='Idade: ', font=('Arial 10'), bg=fundo, fg='white', anchor='w')
+label_idade.grid(row=1, column=0)
 idade = Entry(janela, width=10, font=('Arial 10'))
-idade.grid(row=1, column=0, padx=10, pady=5)
+idade.grid(row=1, column=1, padx=10, pady=5)
 
+# Pais -------------------------------------------------
+label_pais = Label(janela, width=10, height=2, text='Pais: ', font=('Arial 10'), bg=fundo, fg='white', anchor='w')
+label_pais.grid(row=2, column=0)
 pais = Entry(janela, width=10, font=('Arial 10'))
-pais.grid(row=2, column=0, padx=10, pady=5)
+pais.grid(row=2, column=1, padx=10, pady=5)
+
+
 
 janela.mainloop()
