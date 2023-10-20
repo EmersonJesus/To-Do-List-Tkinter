62f4dd4c0446b1be756884a4cc9b30a407cf3aeb
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-20 13:08:09 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -10,6 +10,14 @@ janela.geometry(f'{str(largura)}x{str(altura)}')
 janela.config(bg=fundo)
 janela.resizable(width=False, height=False)
 
+def dados():
+    res_nome['text'] = nome.get()
+    res_idade['text'] = idade.get()
+    res_pais['text'] = pais.get()
+    
+    nome.delete(0, END)
+    idade.delete(0, END)
+    pais.delete(0, END)
 
 # Nome --------------------------------------------
 label_nome = Label(janela, width=10, height=2, text='Nome: ', font=('Arial 10'), bg=fundo, fg='white', anchor='w')
@@ -30,10 +38,15 @@ pais = Entry(janela, width=10, font=('Arial 10'))
 pais.grid(row=2, column=1, padx=10, pady=5)
 
 # Botão ---------------------------------------------------
-botao = Button(janela, width=10, text='Salvar Dados')
+botao = Button(janela, width=10, text='Salvar Dados', command=dados)
 botao.grid(row=3, column=1)
 
 # Dados -----------------------------------------------------
-res_nome
+res_nome = Label(janela, width=10, height=2, text='', font=('Arial 10'), bg=fundo, fg='white', anchor='w')
+res_nome.grid(row=4, column=0)
+res_idade = Label(janela, width=10, height=2, text='', font=('Arial 10'), bg=fundo, fg='white', anchor='w')
+res_idade.grid(row=5, column=0)
+res_pais = Label(janela, width=10, height=2, text='', font=('Arial 10'), bg=fundo, fg='white', anchor='w')
+res_pais.grid(row=6, column=0)
 
 janela.mainloop()
