da86cf94e6add2b94de2072d2b86b4a1b126d69d
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-26 12:52:48 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -11,10 +11,5 @@ janela.geometry(f'{str(largura)}x{str(altura)}')
 janela.config(bg=fundo)
 janela.resizable(width=False, height=False)
 
-radio = Radiobutton(janela, text='Primeiro', value=1)
-radio.grid(row=0, column=0, padx=10, pady=10)
-radio2 = Radiobutton(janela, text='Segundo', value=2)
-radio2.grid(row=1, column=0, padx=10, pady=10)
-radio3 = Radiobutton(janela, text='Terceiro', value=3)
-radio3.grid(row=2, column=0, padx=10, pady=10)
+
 janela.mainloop()
