fa7f0c2e00ce4a22ee3f47698c25dcae11bdb93b
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-26 12:56:04 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,11 +5,12 @@ fundo = '#242323'
 
 janela = Tk()
 janela.title('RadioButton')
-largura = 250
-altura = 250
+largura = 400
+altura = 400
 janela.geometry(f'{str(largura)}x{str(altura)}')
 janela.config(bg=fundo)
 janela.resizable(width=False, height=False)
 
+frame = Frame(janela, width=200, height=200) 
 
 janela.mainloop()
