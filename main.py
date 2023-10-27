7bdf46356a4a2b06b81e9dfa9c062a0b72f392d2
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-27 19:37:28 -0300

    v0.1

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import tkinter
 from tkinter import *
 
-root=Tk()
+root = Tk()
 root.title("To-Do-List")
 root.geometry("400x650+400+100")
 root.resizable(False, False)
 
 lista_tarefas = []
+
+# Icone -------------------
+img_icone = PhotoImage(file='imagens/task.png')
+root.iconphoto(False, img_icone)
+
 
 root.mainloop()
--- teste.py
+++
@@ -1,5 +0,0 @@
-from customtkinter import *
-
-janela = Ctk()
-janela._set_apparence_mode('light')
-janela.mainloop()
